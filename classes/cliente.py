b821a91e0c553962478d4b78c38609bbf4d01ba7
abrothguer <abrothguer@gmail.com>
2019-09-27 11:45:19 -0300

    cadastro e mensagem inicial para o cliente

--- classes/cliente.py
+++ classes/cliente.py
@@ -1,15 +1,20 @@
-import socket                                   # Import socket module
+import socket  # Import socket module
 
-s = socket.socket()                             # Create a socket object
-host = '' # socket.gethostname()                # Get local machine name
-port = 4323                                    # Reserve a port for your service.
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+host = "0.0.0.0"  # socket.gethostname()                # Get local machine name
+port = 4324  # Reserve a port for your service.
 
 s.connect((host, port))
-while True:
+s.send(str.encode(input("Digie seu nome: ")))
+data = s.recv(1024)
 
- s.send(raw_input())
- data = s.recv(1024)
- print (data)
- if data=='SAIR' :
-  break
-s.close()    
+
+while True:
+    data = s.recv(1024)
+    s.send(str.encode(input("Sua ação: ")))
+    data = s.recv(1024)
+    print(data)
+    if data == "SAIR":
+        break
+
+s.close()
--- classes/server.py
+++ classes/server.py
@@ -1,29 +1,65 @@
-import socket                                   # Import socket module
-s = []                             # Create a socket object
-host = '' #socket.gethostname()                     # Get local machine name
-port = 4323 	                                   # Reserve a port for your service.
-                            # Bind to the port
-print ('esperando conexao')
+import socket
 
-c = []
-addr = []
-i=0
-data=[]                                     # Now wait for client connections.
-while i<4:
-   s.append(socket.socket())
-   s[i].bind((host, port+i))
-   s[i].listen(5)
-   aux, aux2 = s[i].accept()
-   c.append(aux)
-   addr.append(aux)
-   data.append( c[i].recv(1024) )
-   print (data[i], 'ENTROU NO JOGO! SUA COR EH')
-# aqui cria a bolinha e retorna a cor da bolinha pro cliente 1
-   x=0
-   while x<i:
-    c[x].send(' entrou no jogo! sua cor eh retorno da funcao que cria a bolinha')
-    x=x+1
- # raw_input() vai ser substituido pela cor da bolinha
-   i=i+1         
-c.close()                                    # Close the connection
+NRO_CLIENTS = 4
 
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+host = "0.0.0.0"  # socket.gethostname()                     # Get local machine name
+port = 4324  # Reserve a port for your service.
+# Bind to the port
+socket.bind((host, port))  # Escutando na pota 4323
+socket.listen(NRO_CLIENTS)  # Conexão com no máximo 4 clients
+print("Esperando conexao")
+
+
+# Classe para guardar o estado do servidor
+class ServerState(object):
+    def __init__(self):
+
+        self.clients = {}
+        self.nro_clients = 0
+        self.next_client = None
+
+    def add_client(self, addr, name, socket):
+
+        self.clients[addr] = (name, socket)
+        self.nro_clients += 1
+
+    def get_client_socket(self, addr):
+
+        return self.clients[addr]
+
+
+state = ServerState()
+
+# Esperando 4 clientes se conectarem
+for _ in range(NRO_CLIENTS):
+
+    conn, addr = socket.accept()  # Aceita conexao, retorna tupla socket, endereco
+    data = conn.recv(1024)
+    state.add_client(addr, data, conn)  # Adiciona o cliente no estado do servidor
+
+    print(data, "ENTROU NO JOGO! SUA COR EH")
+
+    state.next_client = (data, conn) if state.next_client is None else state.next_client
+    # # aqui cria a bolinha e retorna a cor da bolinha pro cliente 1
+    # x = 0
+    # while x < i:
+    #     c[x].send(" entrou no jogo! sua cor eh retorno da funcao que cria a bolinha")
+    #     x = x + 1
+    # # raw_input() vai ser substituido pela cor da bolinha
+    # i = i + 1
+
+# Avisando aos clientes que o jogo começou
+for _, cl_socket in state.clients.values():
+
+    curr_name, curr_conn = state.next_client
+    cl_socket.send(
+        str.encode(
+            f"O jogo começou. \
+            {'Eh a sua vez' if curr_conn == cl_socket else 'E a vez de ' + str(curr_name) }"
+        )
+    )
+
+while True:
+    for _, cl_socket in state.clients.values():
+        cl_socket.send(b"Ola tudo bem?")
