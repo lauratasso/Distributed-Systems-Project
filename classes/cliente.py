d3535e3f0e7eb879d9fb1cef9eab0f8861f837e3
amanda souza <amandasouzac2009@hotmail.com>
2019-09-28 16:54:33 -0300

    Broadcast game

--- classes/cliente.py
+++ classes/cliente.py
@@ -2,44 +2,81 @@ import socket  # Import socket module
 from pong import Tela
 from ball import Ball
 import random
+from threading import Thread
+import json
+
 
 def random_ball():
-	#creating randomly the position of the ball
-	coord_x = random.uniform(10,630)
-	coord_y = random.uniform(10,470)
+    # creating randomly the position of the ball
+    coord_x = random.uniform(10, 630)
+    coord_y = random.uniform(10, 470)
 
-	#choosing randomly the color of the ball
-	r = random.randint(0,255)
-	g = random.randint(0,255)
-	b = random.randint(0,255)
+    # choosing randomly the color of the ball
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
 
-	if r < 25 and g < 25 and b < 25:
-		r = 30
-		g = 30
-		b = 30
+    if r < 25 and g < 25 and b < 25:
+        r = 30
+        g = 30
+        b = 30
 
-	return Ball((r,g,b), coord_x, coord_y)
+    return Ball((r, g, b), coord_x, coord_y)
 
+
+def server_thread():
+    global s
+
+    while True:
+        data = s.recv(1024)
+        try:
+            data_balls = json.loads(data.decode())
+            updateBalls(data_balls)
+        except:
+            pass
+
+        if data == "SAIR":
+            s.close()
+            break
+
+
+def updateBalls(ball_dict):
+    global tela
+
+    for i, value in enumerate(ball_dict.values()):
+        ball = tela.ball_list[i]
+        ball.circle_x, ball.circle_y = value[1], value[2]
+
+        tela.update()
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
-host = "0.0.0.0"  # socket.gethostname()                # Get local machine name
+host = socket.gethostname()                # Get local machine name
 port = 4322  # Reserve a port for your service.
 
 s.connect((host, port))
 s.send(str.encode(input("Digite seu nome: ")))
-data = s.recv(1024) 
+data = s.recv(1024)
 tela = Tela()
 
-tela.ball_list.append(random_ball())
+data_balls = json.loads(data.decode())
+
+for value in data_balls.values():
+    ball = Ball(tuple(value[0]), value[1], value[2])
+    tela.ball_list.append(ball)
+
+thread_conn = Thread(target=server_thread)
+thread_conn.start()
+
 tela.run()
 
-while True:
-    data = s.recv(1024)
-    #s.send(str.encode(input("Sua ação: ")))
-    #data = s.recv(1024)
-    print(data)
-    if data == "SAIR":
-        break
 
-s.close()
+# print("OI")
+
+# while True:
+#     data = s.recv(1024)
+#     #s.send(str.encode(input("Sua ação: ")))
+#     #data = s.recv(1024)
+#     print(data)
+#     if data == "SAIR":
+#         break
--- classes/server.py
+++ classes/server.py
@@ -1,8 +1,13 @@
 import socket
+import random
+import time
+import json
 
 NRO_CLIENTS = 2
+INTERVAL = 0.08
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+# Create a socket object
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "0.0.0.0"  # socket.gethostname()                     # Get local machine name
 port = 4322  # Reserve a port for your service.
 # Bind to the port
@@ -18,6 +23,7 @@ class ServerState(object):
         self.clients = {}
         self.nro_clients = 0
         self.next_client = None
+        self.client_balls = []
 
     def add_client(self, addr, name, socket):
 
@@ -28,6 +34,70 @@ class ServerState(object):
 
         return self.clients[addr]
 
+    def get_balls_pos(self, update):
+
+        json_dict = {}
+        for i, (data, _) in enumerate(self.clients.values()):
+
+            if(update):
+                x, y, sx, sy = move(self.client_balls[i][1], self.client_balls[i]
+                                    [2], self.client_balls[i][3], self.client_balls[i][4])
+
+                self.client_balls[i][1], self.client_balls[i][2] = x, y
+                self.client_balls[i][3], self.client_balls[i][4] = sx, sy
+            json_dict[data.decode()] = self.client_balls[i]
+
+        return json.dumps(json_dict)
+
+
+def random_ball():
+    # creating randomly the position of the ball
+    coord_x = random.uniform(10, 630)
+    coord_y = random.uniform(10, 470)
+
+    # choosing randomly the color of the ball
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    if r < 40 and g < 40 and b < 40:
+        r = 45
+        g = 45
+        b = 45
+
+    speed_x = 250.
+    speed_y = 250.
+
+    return [(r, g, b), coord_x, coord_y, speed_x, speed_y]
+
+
+def move(circle_x, circle_y, speed_x, speed_y):
+
+    # time_passed = clock.tick(30)
+    # time_sec = time_passed / 1000.0
+
+    time_sec = INTERVAL
+
+    circle_x += speed_x * time_sec
+    circle_y += speed_y * time_sec
+
+    if circle_x <= 10.:
+        if circle_y >= 7.5 and circle_y <= 470:
+            circle_x = 20.
+            speed_x = -speed_x
+    if circle_x >= 630.:
+        if circle_y >= 7.5 and circle_y <= 470:
+            circle_x = 605.
+            speed_x = -speed_x
+    if circle_y <= 10.:
+        speed_y = -speed_y
+        circle_y = 10.
+    elif circle_y >= 457.5:
+        speed_y = -speed_y
+        circle_y = 457.5
+
+    return circle_x, circle_y, speed_x, speed_y
+
 
 state = ServerState()
 
@@ -36,11 +106,16 @@ for _ in range(NRO_CLIENTS):
 
     conn, addr = socket.accept()  # Aceita conexao, retorna tupla socket, endereco
     data = conn.recv(1024)
-    state.add_client(addr, data, conn)  # Adiciona o cliente no estado do servidor
+    # Adiciona o cliente no estado do servidor
+    state.add_client(addr, data, conn)
 
     print(data, "ENTROU NO JOGO! SUA COR EH")
 
-    state.next_client = (data, conn) if state.next_client is None else state.next_client
+    state.next_client = (
+        data, conn) if state.next_client is None else state.next_client
+
+    ball = random_ball()
+    state.client_balls.append(ball)
     # # aqui cria a bolinha e retorna a cor da bolinha pro cliente 1
     # x = 0
     # while x < i:
@@ -50,19 +125,18 @@ for _ in range(NRO_CLIENTS):
     # i = i + 1
 
 # Avisando aos clientes que o jogo começou
-for _, cl_socket in state.clients.values():
+for i, (_, cl_socket) in enumerate(state.clients.values()):
 
     curr_name, curr_conn = state.next_client
-    cl_socket.send( "OK!!!!!"
-       # str.encode(
-    #        f"O jogo começou. \
-     #       {'Eh a sua vez' if curr_conn == cl_socket else 'E a vez de ' + str(curr_name) }"
-      #  )
+    cl_socket.send(
+        str.encode(
+            f"{state.get_balls_pos(False)}"
+        )
     )
-ball = Ball((255,0,0),423,212)
 
-#while True:
- #   for _, cl_socket in state.clients.values():
-
-  #      cl_socket.send(b"Ola tudo bem?")
-
+time.sleep(2)
+while True:
+    time.sleep(INTERVAL)
+    result = state.get_balls_pos(True)
+    for _, cl_socket in state.clients.values():
+        cl_socket.send(result.encode())
