d6f3abd499c982f4f554a7116b1e748540f2b4f8
lauratasso <33038813+lauratasso@users.noreply.github.com>
2019-10-01 19:37:09 -0300

    Add files via upload

--- src/utils/color.py
+++ src/utils/color.py
@@ -3,8 +3,9 @@ GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 BLUE = (0, 0, 255)
 VIOLET = (128, 0, 128)
-ORANGE = (255, 255, 0)
+LIGHT_CYAN = (64,224,208)
 WHITE = (255, 255, 255)
 
-colors = [["vermelho", RED], ["verde", GREEN], ["amarelo", YELLOW],
-          ["azul", BLUE], ["roxo", VIOLET], ["laranja", ORANGE], ["branco", WHITE]]
+colors = [["vermelho", RED, "\033[31m"], ["verde", GREEN, "\033[32m"], ["amarelo", YELLOW, "\033[33m"],
+          ["azul", BLUE, "\033[34m"], ["roxo", VIOLET,"\033[35m"], ["cyan claro", LIGHT_CYAN, "\033[31m"],
+          ["branco", WHITE, "\033[37m"]]
